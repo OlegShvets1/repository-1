bc0242e51df7ce372614997e3cca1f92a69f5370
unknown <olehhpost69@gmail.com>
2022-08-01 19:07:05 +0300

    виправлене 8 завдання

--- lesson2/src/Task8.java
+++ lesson2/src/Task8.java
@@ -1,8 +1,7 @@
 public class Task8 {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            if (i % 3!=0)  continue;
-            if (i % 5!=0)  continue;
+            if (i % 3!=0 && i % 5!=0) continue;
             if (i % 2!=1)  continue;
             System.out.println("i = " + i);
         }
